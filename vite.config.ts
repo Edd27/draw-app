e43fdd5efad1ecfe22b9a4b8b3722e63b8f283d2
Edgar Benavides <edgarben27@gmail.com>
2025-07-02 14:12:21 -0600

    fix: corregir enlaces de estilos y título en index.html; agregar configuración de vista previa en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 4173,
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
